1abab086a77164d8761dcd7c6ee82c558c7c5aee
Al Miller <albert.miller@fireflyautomatix.com>
2021-07-02 07:34:14 -0700

    adding dev

--- view/src/App.vue
+++ view/src/App.vue
@@ -1,31 +1,50 @@
 <template>
   <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <button @click="send('hey')">Send message</button>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button
+      style="
+        color: white;
+        font-size: 90px;
+        background: red;
+        width: 500px;
+        height: 500px;
+      "
+      @click="send('hey')"
+    >
+      Send message
+    </button>
+    <!-- <HelloWorld /> -->
   </div>
 </template>
 
 <script setup>
 import HelloWorld from "./components/HelloWorld.vue";
 
-console.log(window.ipcRenderer)
+// console.log(window.ipcRenderer)
 import sendAsync from "./message-control/renderer";
 
-// console.log(sendAsync);
+console.log(sendAsync);
 
 function send(data) {
   sendAsync(data).then((result) => {
     console.log(result);
   });
 }
+async function addFarm({ commit }, farm) {
+  // Check db.json
+  // Add keys to farm (more convienent than? remove?)
+  const augmentedFarm = Object.assign(farm, {
+    id: uuid(),
+  });
+  const response = await axios
+    .post(`/api/farms`, augmentedFarm)
+    .catch((error) => console.log);
+  // TODO: Also accept 201?
+  if (response.status === 200) {
+    commit("ADD_FARM", augmentedFarm);
+  }
+}
 </script>
 
-// <script>
-// export default {
-//   setup() {},
-// };
-// </script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
