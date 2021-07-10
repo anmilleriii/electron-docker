c2e4acbb3f325997684b9b0703dfefc49159f5cf
Al Miller <albert.miller@fireflyautomatix.com>
2021-07-09 17:14:55 -0700

    in progress

--- view/README.md
+++ view/README.md
@@ -5,3 +5,11 @@
 
 ## Run Container (Linux)
 `cd docker && ./start.sh`
+
+
+## Linux (linux/amd64
+
+
+| Item | OS | Architecture | sdfsdf |
+| --- | --- | ------ | ------ |
+| Panasonic F1-GZ tablet | Ubuntu 20.04 Linux | linux/arm64 | sdf    |
--- view/docker/start.sh
+++ view/docker/start.sh
@@ -1,4 +1,12 @@
 # This is not the safest way however, as you then compromise the access control to X server on your host
+
+
+# Architectures
+# 
+
+
+
+
 # xhost +local:root # for the lazy and reckless
 # docker run -it --env="DISPLAY" --network="host" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" view
 # export containerId=$(docker ps -l -q)
@@ -12,5 +20,21 @@
 # -e DISPLAY=host.docker.internal:0
 # --network="host" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" 
 xhost +local:root
-docker run --rm -it --env DISPLAY=host.docker.internal:0 -p 8080:8080 -d view
+# docker run --rm -it --env DISPLAY=host.docker.internal:0 -p 8080:8080 -d view
+
+
+
+
+docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag am/view:buildx-latest .
 # apt install x11-apps
+# docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+# docker buildx bake --set '*.platform=linux/amd64,linux/arm64' --push
+
+# docker buildx create --name emulated-multiarch --driver docker-container --platform=linux/amd64,linux/arm64 --use
+
+docker buildx build --push --platform linux/arm64/v7 --tag am/view:buildx-latest .
+# docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag am/view:buildx-latest .
+
+
+# docker buildx prune
+# docker system prune
